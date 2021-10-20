fb04c6ef4feb504a00e62891b58aafeb20b25802
Rutuja095 <ritujashah19@gmail.com>
2021-10-20 22:45:57 +0530

    feat(): checkout

---
+++ src/components/Checkout.js
@@ -0,0 +1,123 @@
+<div id="checkout">
+		<div class="container">
+			{/* <!-- MultiStep Form -->
+		    <!-- <div class="row justify-content-center mt-0"> -->
+		        <!-- <div class="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2"> --> */}
+		            <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
+		                <div class="row">
+		                    <div class="col-md-12 mx-0">
+		                        <form id="msform">
+		                            {/* <!-- progressbar --> */}
+		                            <ul id="progressbar">
+		                                <li class="active" id="account"><strong>My Cart</strong></li>
+		                                <li id="personal"><strong>Billing Address</strong></li>
+		                                <li id="payment"><strong>Payment</strong></li>
+		                            </ul> 
+		                            {/* <!-- fieldsets --> */}
+		                            <div class="row">
+			                            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 order-md-first order-last">
+				                            <fieldset>
+				                            	<h2 class="fs-title">My Cart</h2> 
+				                                <div class="form-card">
+				                                    <div class="h5">
+				                                    	<span>Product name</span><span style="float:right">$12</span>
+				                                    </div>
+				                                    <div class="h6">Vat : <span>45</span></div>
+				                                    <div class="h6">QTY : <span>45</span></div>
+				                                </div> 
+				                                <div class="form-card">
+				                                    <div class="h5">
+				                                    	<span>Product name</span><span style="float:right">$12</span>
+				                                    </div>
+				                                    <div class="h6">Vat : <span>45</span></div>
+				                                    <div class="h6">QTY : <span>45</span></div>
+				                                </div> 
+				                                <input type="button" name="next" class="next action-button" value="Next" />
+				                            </fieldset>
+				                            <fieldset>
+				                            	<h2 class="fs-title">Billing Address</h2> 
+				                                <div class="form-card">
+				                                    <input type="text" name="fname" placeholder="First Name" /> 
+				                                    <input type="text" name="lname" placeholder="Last Name" /> 
+				                                    <input type="text" name="email" placeholder="Email" /> 
+				                                    <input type="text" name="phno" placeholder="Contact No." /> 
+				                                    <input type="text" name="instaid" placeholder="Instagram Name" /> 
+				                                    <input type="text" name="delivery_address" placeholder="Delivery Address" />
+				                                    <input type="text" name="postcode" placeholder="Post Code" /> 
+				                                    <label class="h5">Trade of business</label>
+				                                    <select class="select" id="trade_business" name="trade_business">
+		                                                <option selected></option>
+		                                            </select> 
+				                                </div> 
+				                                <input type="button" name="previous" class="previous action-button-previous" value="Back" /> 
+				                                <input type="button" name="next" class="next action-button" value="Proceed to Pay" />
+				                            </fieldset>
+				                            <fieldset>
+				                            	<h2 class="fs-title">Payment Information</h2>
+				                                <div class="form-card">
+				                                    <div class="radio-group">
+				                                        <div class="h5">
+				                                        	<div class='radio' data-value="credit"></div>Credit Card
+				                                        </div>
+				                                        <div class="h5">
+				                                        	<div class='radio' data-value="paypal"></div>Paypal
+				                                    	</div>
+				                                        <div class="h5">
+				                                        	<div class='radio' data-value="paypal"></div>Pay on Delivery
+				                                        </div>
+				                                    </div>
+				                                </div> 
+				                                <input type="button" name="previous" class="previous action-button-previous" value="Back" /> 
+				                                <input type="button" name="make_payment" class="next action-button" value="Confirm Order" />
+				                            </fieldset>
+				                            <fieldset>
+				                                <div class="form-card">
+				                                    <div><h2 class="fs-title text-center">Success!</h2></div>
+				                                    <div class="row justify-content-center">
+				                                        <div class="col-3"> 
+				                                        	<img src="https://img.icons8.com/color/96/000000/ok--v2.png" class="fit-image" /> 
+				                                        </div>
+				                                    </div>
+				                                    <div class="row justify-content-center">
+				                                        <div class="col-7 text-center">
+				                                            <h5>Your Order Placed Sucessfully</h5>
+				                                        </div>
+				                                    </div>
+				                                </div>
+				                            </fieldset>
+			                            </div>
+			                            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			                            	<fieldset>
+			                            		<h2 class="fs-title">Amount Information</h2> 
+				                                <div class="form-card" style="background-color: #7984a3;color: #fff">
+				                                    <div class="h5">
+				                                    	<span>Subtotal</span><span style="float:right">$12</span>
+				                                    </div>
+				                                    <div class="h6">Total Vat : <span style="float:right">$0</span></div>
+				                                    <div class="row">
+					                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					                                    	<label class="h6">Shipping Cost</label>
+					                                    </div>
+					                                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+					                                    	<select class="select" id="shipping" name="shipping">
+					                                            <option selected>Next day delivery 9.98GBP</option>
+					                                        </select> 
+					                                    </div>
+					                                </div>
+				                                    <div class="separator"></div>
+				                                    <div class="h5">
+				                                    	<span>Total</span><span style="float:right">$12</span>
+				                                    </div>
+				                                </div> 
+				                            </fieldset>
+			                            </div>
+			                        </div>
+		                        </form>
+		                    </div>
+		                </div>
+		            </div>
+		        {/* <!-- </div> -->
+		    <!-- </div> --> */}
+
+		</div>
+	</div>
