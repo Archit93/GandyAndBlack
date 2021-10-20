6f57b77803d7109c3792b715f634a2ddd6959479
Rutuja095 <ritujashah19@gmail.com>
2021-10-20 22:28:36 +0530

    feat(): aboutus signin forgotpassword

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -141,9 +141,13 @@ const SignIn = (props) => {
 				<div id="login">
 					<div role="main">
 						<form method="post">
-							<div className="form">
-								<label for="login-email" className="label">Email</label>
-								<input id="login-email" type="email" required />
+							<div class="form-floating mb-3">
+								<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+								<label for="floatingInput">Email address</label>
+								</div>
+								<div class="form-floating">
+								<input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+								<label for="floatingPassword">Password</label>
 							</div>
 							<div className="form">
 								<label for="login-password" className="label">Password</label>
