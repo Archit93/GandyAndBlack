a29304e7e8004e4139009184f873ac66fac210db
Archanavish12 <archanavish12@gmail.com>
2021-11-02 01:06:34 +0000

    feat(billing): added billing address flow

--- src/utils/regexUtils.js
+++ src/utils/regexUtils.js
@@ -3,6 +3,7 @@ export const regexes = {
   name: /\b([A-Za-zÀ-ÿ][-,a-z. ']+[ ]*)+/,
   email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   password: /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,}))/,
+  phone: /^(07[\d]{9})$/,
   postcode:
     /^((GIR 0AA|X9 9RB|X9 9AA|X9 9BF|X9 9AJ|X9 9AB|X9 9TA|X9 9LG|X9 9BH|X9 9LF|X9 2TR|XE16 4QZ|XE11 1AA|XE98 1TL|XEP O11|XE99 3GG|XE0 2AQ|XE1A 1AA|XE18 3AN|XE7 4GJ|XE1P 2PR|XE2 7EJ|XE12 8JS|XE1H 9DW|XE 5GG|XE1X 7XA|XE1X 7XF|XE1 8NH|XE1 1RJ|XE1 1RN|XE1 1LE|XE24 99X|XE 0AA|XE12 8HJ)|((([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))) [0-9][A-Z]{2}))$/i,
 };
@@ -43,6 +44,14 @@ export const isValidPostcode = (value) => {
   const bool =
     value && !regexes.postcode.test(value)
       ? "This looks like an invalid postcode. Could you please check?"
+      : undefined;
+  return bool;
+};
+
+export const isValidPhone = (value) => {
+  const bool =
+    value && !regexes.phone.test(value)
+      ? "The phone number looks incorrect. It should start with 0 and must have 11 digits"
       : undefined;
   return bool;
 };
