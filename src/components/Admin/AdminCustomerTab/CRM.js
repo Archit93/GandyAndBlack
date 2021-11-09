b84f31e6068e9a3dfc4d9c472de605467235310f
Archit93 <architmehta007@hotmail.com>
2021-11-09 01:41:11 +0000

    feat(): on card click of crm card

--- src/components/Admin/AdminCustomerTab/CRM.js
+++ src/components/Admin/AdminCustomerTab/CRM.js
@@ -6,8 +6,19 @@ import Board from "react-trello";
 // import CustomCard from "./CustomCard.js";
 import {Sidebar} from './Sidebar.js';
 
+import PromoCodeModal from './PromoCodeModal';
+
 const CRM = (props) => {
 
+  const [showModal, setShowModal] = React.useState(false);
+
+  const showPromocodeModal = (showModalValue) => {
+    setShowModal(showModalValue);
+}
+
+  const onCardClick = (cardId, metadata, laneId) => {
+    showPromocodeModal(true);
+  }
     return (
         <div className="App">
           {/* <Sidebar /> */}
@@ -17,10 +28,14 @@ const CRM = (props) => {
               data={data} 
               style={{ height: 'calc(100vh - 57px)'}}
               cardDraggable={false} 
-              hideCardDeleteIcon 
+              hideCardDeleteIcon
+              onCardClick={onCardClick} 
               >
               </Board>
               </div>
+              <PromoCodeModal title="My Modal" onClose={() =>  showPromocodeModal(false)} show={showModal}>
+        <p>This is modal body</p>
+      </PromoCodeModal>
         </div>
       );
     
