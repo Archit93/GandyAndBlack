08f907bec35d62756fbb9a4511d518c71e1a887f
Archit93 <architmehta007@hotmail.com>
2021-11-24 00:42:40 +0000

    feat(): Products Ordered Details

--- src/components/Admin/CRMTab/ProductDetailsTab.js
+++ src/components/Admin/CRMTab/ProductDetailsTab.js
@@ -1,50 +1,106 @@
-import * as React from "react";
+import React from 'react';
+import { AgGridReact } from 'ag-grid-react';
 
-const ProductDetailsTab = (props) => {
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-    return (<><div className="row form-card mt-5">
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-  </div>
-  <div className="row form-card mt-3">
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-  </div>
-  <div className="row form-card mt-3">
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-  </div>
-  <div className="row form-card mt-3">
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-  </div>
-  <div className="row form-card mt-3">
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-    <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <label><strong>Order Id -</strong></label> SDE4242
-            </div>
-  </div>
-  <div className="text-align-center mrt-20">
-    <button className="btn btn-primary">Save</button>
-  </div>
-        </>)
+import { ProductQuantityEditor } from './ProductQuantityEditor';
+import { ProductVatEditor } from './ProductVatEditor';
+import { SalesPricePerUnitEditor } from './SalesPricePerUnitEditor';
+import {TotalAmountEditor} from './TotalAmountEditor';
+import {TotalAmountPaidEditor} from './TotalAmountPaidEditor';
+
+
+const ProductDetailsTab = ({ orderInfo }) => {
+
+  const [gridApi, setGridApi] = React.useState(null);
+  const [gridColumnApi, setGridColumnApi] = React.useState(null);
+
+  const onGridReady = params => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi)
+    params.api.sizeColumnsToFit()
+  }
+
+  const frameWorkComponentChange = ({ api }) => {
+
+  }
+
+  const columnDefs = ({ frameWorkComponentChange }) => [
+    { field: 'brand', headerName: "Brand" },
+    { field: 'producttype', headerName: "Product Type" },
+    { field: 'productdesc', headerName: "Product Description" },
+    {
+      field: 'quantity',
+      headerName: "Product Quantity",
+      cellEditor: "productQuantityEditor",
+      editable: true
+    },
+    {
+      field: 'price',
+      headerName: "Sales Price Unit",
+      cellEditor: "salesPricePerUnitEditor",
+      editable: true
+    },
+    {
+      field: 'vat',
+      headerName: "Product vat",
+      cellEditor: "productVatEditor",
+      editable: true
+    },
+    {
+      field: 'amount',
+      headerName: "Total",
+      cellEditor: "totalAmountEditor",
+      editable: true
+    },
+    {
+      field: 'amountpaid',
+      headerName: "Amount Paid",
+      cellEditor: "totalAmountPaidEditor",
+      editable: true
+    },
+  ];
+
+  const rowData = () => {
+    const productOrderedListArray = [];
+    orderInfo.orderdetailList.map((rowdetail) => {
+      productOrderedListArray.push({
+        ...rowdetail
+      })
+    });
+    return productOrderedListArray
+  };
+
+
+  const defaultColDef = React.useMemo(() => ({
+    resizable: true,
+    sortable: true
+  }), []);
+
+  return (<>
+    <div
+      className="ag-theme-alpine"
+      style={{ height: "calc(100vh - 315px)", width: "100%" }}
+    >
+      <AgGridReact
+        rowData={rowData()}
+        columnDefs={columnDefs({
+          frameWorkComponentChange: frameWorkComponentChange,
+        })}
+        defaultColDef={defaultColDef}
+        onGridReady={onGridReady}
+        context={{ frameWorkComponentChange: frameWorkComponentChange }}
+        frameworkComponents={{
+          productQuantityEditor: ProductQuantityEditor,
+          productVatEditor: ProductVatEditor,
+          salesPricePerUnitEditor: SalesPricePerUnitEditor,
+          totalAmountEditor: TotalAmountEditor,
+          totalAmountPaidEditor: TotalAmountPaidEditor
+        }}
+      ></AgGridReact>
+    </div>
+  </>)
+
 }
 export default ProductDetailsTab
