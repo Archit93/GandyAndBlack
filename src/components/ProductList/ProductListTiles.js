6f76f3cb7ad6027b5956830bd57e306d1183a9a6
Archit93 <architmehta007@hotmail.com>
2021-11-26 16:18:48 +0000

    feat(): product Tiles

---
+++ src/components/ProductList/ProductListTiles.js
@@ -0,0 +1,64 @@
+import React from "react";
+import HeaderMenu from "../common/HeaderMenu";
+
+const ProductListTiles = (props) => {
+    return (
+        <>
+            <div>
+                <HeaderMenu />
+            </div>
+            <div
+                className="container-fluid"
+                style={{ width: "100%", height: "100%" }}
+            >
+                <div className="row">
+                    <button className="col-lg-4 tile-btn">
+                        <div className="rectangle-202-uEBdwN">
+                            <div className="poppins-normal-white-24px">
+                                SHOP DERMAL FILLERS
+		        </div>
+                        </div>
+                    </button>
+                    <button className="col-lg-4 tile-btn">
+                        <div className="rectangle-203-uEBdwN">
+                            <div className="poppins-normal-white-24px">
+                                SHOP BODY FILLERS
+	          	</div>
+                        </div>
+                    </button>
+                    <button className="col-lg-4 tile-btn">
+                        <div className="rectangle-204-uEBdwN">
+                            <div className="about-gandy-black-uEBdwN poppins-normal-white-24px">
+                                SHOP SKIN BOOSTERS
+	          	</div>
+                        </div>
+                    </button>
+                </div>
+                <div className="row">
+                    <button className="col-lg-4 tile-btn">
+                        <div className="rectangle-205-uEBdwN">
+                            <div className="poppins-normal-white-24px">
+                                SHOP FAT DISSOLVERS
+		        </div>
+                        </div>
+                    </button>
+                    <button className="col-lg-4 tile-btn">
+                        <div className="rectangle-201-uEBdwN">
+                            <div className="poppins-normal-white-24px">
+                                SHOP CONSUMABLES
+	          	</div>
+                        </div>
+                    </button>
+                    <button className="col-lg-4 tile-btn">
+                        <div className="rectangle-206-uEBdwN">
+                            <div className="about-gandy-black-uEBdwN poppins-normal-white-24px">
+                                SHOP EXTRAS
+	          	</div>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+};
+export default ProductListTiles;
