f09c214d4e9bea37760b6b6d9bfe7569777cce0d
Archit93 <34236332+Archit93@users.noreply.github.com>
2021-12-31 12:04:44 +0000

    feat():crm scroll and product details height bug fixed (#73)
    Co-authored-by: Rutuja095 <ritujashah19@gmail.com>

--- src/components/Admin/CRMTab/ProductDetailsTab.js
+++ src/components/Admin/CRMTab/ProductDetailsTab.js
@@ -125,7 +125,7 @@ const ProductDetailsTab = ({ orderInfo, showEmailPopUp, dispatch, history, confi
     <div
       className="ag-theme-alpine product-detail-table"
       // style={{ height: "calc(100vh - 315px)", width: "100%" }}
-      style={{ height: "520px", width: "100%" }}
+      style={{ width: "100%" }}
     >
       <AgGridReact
         rowData={productsOrderedArray}
